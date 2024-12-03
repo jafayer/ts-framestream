dce05400f9c29bd61eaa9e835018ec2ca7f4f659
jafayer <github@fayer.me>
2024-12-02 21:19:12 -0500

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/*.(spec|test).(ts|js)'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
