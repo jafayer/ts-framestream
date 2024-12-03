50c172bd005b657f282cf9c74de80f08d0ea7f64
jafayer <github@fayer.me>
2024-12-02 23:00:15 -0500

    README changes and add a helper method

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ A lightweight, pure-TypeScript implementation of the [Frame Stream protocol](htt
 
 Other Node.js implementations of the protocol seem to have stopped short of providing full implementations of the handshaking and implementation-specific aspects of the Frame Stream protocol, necessary in building applications compatible with standards such as Dnstap.
 
+This aims to provide all the methods, types, and helpers needed to build protocol-compatible Frame Stream applications.
+
 ## Acknowledgements
 
 This implementation was heavily inspired by [golang-framestream](https://github.com/farsightsec/golang-framestream/) and [go-framestream](https://github.com/dmachard/go-framestream).
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,47 @@
-import { Socket } from 'net';
+import net from 'net';
 import { Encoder } from './encoder';
 import { ControlFrameType } from './constants';
-export {
-    ControlFrameType,
-    ControlFieldType,
-    FrameType,
-    ESCAPE_SEQUENCE,
-} from "./constants";
+export { ControlFrameType, ControlFieldType, FrameType, ESCAPE_SEQUENCE } from './constants';
 
 export type WriterOpts = {
-  socket: Socket;
-  encoder: Encoder;
-  bidirectional: boolean;
+  socket: net.Socket;
+  encoder?: Encoder;
+  bidirectional?: boolean;
   contentTypes?: string[];
 };
 
+export type ConnectionOpts = Omit<WriterOpts, 'socket'> & {
+  host: string;
+  port: number;
+};
+
 export class Writer {
-  private socket: Socket;
+  private socket: net.Socket;
   private encoder: Encoder;
   private bidirectional: boolean;
   private contentTypes?: string[];
 
-  constructor({ socket, encoder, contentTypes, bidirectional = true }: WriterOpts) {
+  constructor({ socket, encoder = new Encoder(), contentTypes, bidirectional = true }: WriterOpts) {
     this.socket = socket;
     this.encoder = encoder;
     this.bidirectional = bidirectional;
     this.contentTypes = contentTypes;
+  }
+
+  /**
+   * Connect to a server on a given endpoint and return a Writer instance.
+   */
+  static async FromEndpoint({
+    host,
+    port,
+    contentTypes,
+    bidirectional = true,
+    encoder = new Encoder(),
+  }: ConnectionOpts): Promise<Writer> {
+    const socket = await encoder.connect(port, host);
+    const writer = new Writer({ socket, encoder, contentTypes, bidirectional });
+    await writer.init();
+    return writer;
   }
 
   async init() {
